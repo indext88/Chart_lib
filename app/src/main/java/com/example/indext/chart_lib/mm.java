1d6b1b9d53679d8e0a79d781ca63cdf2bc938c51
YourName <YourName@mindmini.com>
2019-04-03 20:04:42 +0800

    第二次更改

--- app/src/main/java/com/example/indext/chart_lib/mm.java
+++ app/src/main/java/com/example/indext/chart_lib/mm.java
@@ -2,6 +2,6 @@ package com.example.indext.chart_lib;
 
 public class mm {
     public static void main(String[] args) {
-        System.out.println("您是奶粉");
+        System.out.println("您是奶粉dededed");
     }
 }
